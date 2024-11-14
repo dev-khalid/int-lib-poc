9d2aec5de952e04f785cae9f9e060b7f2ed5ed33
dev-khalid <khalid.hossain.akash@gmail.com>
2024-11-15 05:34:14 +0600

    easy export

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,7 +1,2 @@
-// import {SharedModule} from './shared.module'; 
-import { SharedService } from './shared.service';
-
-export {
-  SharedService,
-  // SharedModule,
-}
+export * from './shared.module'; 
+export * from './shared.service';
--- libs/worker/src/index.ts
+++ libs/worker/src/index.ts
@@ -1,7 +1,2 @@
-import { WorkerModule } from "./worker.module";
-import { WorkerService } from "./worker.service";
-
-export {
-  WorkerService,
-  WorkerModule,
-}
+export * from './worker.module';
+export * from './worker.service';
